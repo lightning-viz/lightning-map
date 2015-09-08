e0db013ded59e35af6faa3dc493d9f91c3cec7de
freeman <the.freeman.lab@gmail.com>
2015-09-08 00:28:08 -0400

    Fix example

--- data/example.scala
+++ data/example.scala
@@ -3,8 +3,8 @@ import scala.util.Random
 
 val lgn = Lightning()
 
-val states = ["NA", "AK", "AL", "AR", "AZ", "CA", "CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"]
+val states = ["NA", "AK", "AL", "AR", "AZ", "CA", "CO","CT","DC","DE"]
 
 val values = Array.fill(states.length)(Random.nextFloat())
 
-lgn.map(states, values=values)
+lgn.map(states, values, colormap="Blues")
